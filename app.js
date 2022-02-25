13324a91efa832f34b42b02ac9dc6ca5af19fa9a
gpmike31 <gpmike3105@gmail.com>
2022-02-24 19:05:37 -0600

    edit multi-line string function 9.2.4

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
 //output function
-const printProfileData = profileDataArr => {
+//const printProfileData = profileDataArr => {
     //This....
-    for (let i = 0; i < profileDataArr.length; i++) {
-      console.log(profileDataArr[i]);
-    }
+    //for (let i = 0; i < profileDataArr.length; i++) {
+      //console.log(profileDataArr[i]);
+   // }
 
-    console.log('================');
+  //  console.log('================');
 
     // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+  //  profileDataArr.forEach(profileItem => console.log(profileItem));
+//};
+
+const generatePage = (userName, githubName) => {
+    return `
+    Name: ${Gerald}
+    Github: ${geraldhub}
+    `;
 };
+
+console.log(generatePage('Gerald', 'geraldhub'));
+
